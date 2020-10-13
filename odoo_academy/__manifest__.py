4f210a3751aa968762124c1e116d71028d75dbab
John Lee <John@AssistiveIntelligence.com>
2020-10-13 11:35:08 -0400

    Added odoo academy module.

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Acadamy app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'John',
+
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        'demo/academy_demo.xml',
+    ],
+}
