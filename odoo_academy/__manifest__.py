3f620d73d20ef02a713f678f359b3d29164cd14c
JLeeTelescopeCasual <JLee@TelescopeCasual.com>
2020-10-14 17:02:45 +0000

    Made up for previous checkins. Added session model. Added security for session model.

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -21,7 +21,10 @@
     'depends': ['base'],
     
     'data': [
-        
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
     ],
     
     'demo': [
